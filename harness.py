7887dd1ecc70b5609031cf03af5f0197a82f833a
veryslowcode <veryslowcode@gmail.com>
2024-08-21 18:37:44 -0600

    Added configuration parsing.

--- harness.py
+++ harness.py
@@ -26,15 +26,34 @@ class Arguments:
     file: str = "harness.conf"
 
 
+@dataclass
+class Configuration:
+    keywords: list  # Indexed list of keywords to match
+    colors:   list  # Indexed list of colors synchronized with keywords
+    baseColor: int  # Indicates the color that non-matched output will be
+    hasBase:  bool  # Flag to indicate if a base color was defined
+
+
+def handled(func):
+    ''' Simple error handling
+    decorator to exit early '''
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            # Style the output for easier detection
+            print(f"{ERR_PREFIX}{e}{RST_SUFFIX}")
+            exit(1)
+    return wrapper
+
+
 def main() -> None:
-    try:
-        arguments = set_arguments()
-    except argparse.ArgumentError:
-        exit(1)
-
-    print(arguments)
+    arguments = set_arguments()
+    configuration = set_configuration(arguments.file)
+    print(configuration)
 
 
+@handled
 def set_arguments() -> Arguments:
     description = "Capture and colorize log output"
     parser = argparse.ArgumentParser(description=description)
@@ -67,9 +86,8 @@ def set_arguments() -> Arguments:
         else:
             # Raising an exception is better feedback on usage
             # vs. default/silent handling
-            error = argparse.ArgumentTypeError(
-                    f"{ERR_PREFIX}Invalid mode! "
-                    + f"'word' or 'line' expected{RST_SUFFIX}")
+            error = argparse.ArgumentTypeError("Invalid mode! " +
+                                               "'word' or 'line' expected")
             parser.print_help()
             print('\n')
             raise error
@@ -77,6 +95,44 @@ def set_arguments() -> Arguments:
     return arguments
 
 
+@handled
+def set_configuration(file: str) -> Configuration:
+    handle = open(file, "r")  # This may throw but is handled by decorator
+    content = handle.readlines()
+    configuration = Configuration([], [], 0, False)
+
+    for line in content:
+        line = line.strip()
+
+        # Easy enough to handle comments
+        # shouldn't be indented though
+        if line == "" or line[0] == "#":
+            continue
+
+        values = line.split("=")
+        if len(values) < 2:
+            raise Exception("Configuration must be in the "
+                            "following format [KEY]=[COLOR]")
+
+        keyword = values[0].strip()
+
+        try:
+            # Though not necessary, if the configuration
+            # is invalid, this will produce a helpful
+            # indication vs. it just not working/coloring
+            color = int(values[1].strip())
+        except Exception:
+            raise Exception("Color must be an integer")
+
+        if keyword.lower() == "base":
+            configuration.hasBase = True
+            configuration.baseColor = color
+        else:
+            configuration.keywords.append(keyword)
+            configuration.colors.append(color)
+
+    return configuration
+
+
 if __name__ == '__main__':
     main()
-
