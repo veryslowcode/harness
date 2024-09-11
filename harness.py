3c4db6a86bfac12fe9758d4a542a4c07001be588
veryslowcode <veryslowcode@gmail.com>
2024-09-10 19:46:01 -0600

    Refactor and added vim folds for organization.

--- harness.py
+++ harness.py
@@ -16,47 +16,43 @@ RST_SUFFIX = f"{CSI}0m"  # Reset suffix
 
 
 class Mode(Enum):
+    # Mode {{{
     LINE = 0  # Matching word will color the whole line
     WORD = 1  # Matching words will be colored separately
+    # }}}
 
 
 class Style(Enum):
+    # Style {{{
     Bit4 = 0  # Uses 4 bit Ansi style output
     Bit8 = 1  # Uses 8 bit Ansi style output
     Bit24 = 2  # Uses RGB Ansi style output
+    # }}}
 
 
 @dataclass
 class Arguments:
+    # Arguments {{{
     command: list  # Command is required, the rest are optional
     ignore:  bool = False
     mode:     int = Mode.LINE
     style:    int = Style.Bit8
     file:     str = "harness.conf"
+    # }}}
 
 
 @dataclass
 class Configuration:
+    # Configuration {{{
     keywords: list  # Indexed list of keywords to match
     colors:   list  # Indexed list of colors synchronized with keywords
     baseColor: str  # Indicates the color that non-matched output will be
     hasBase:  bool  # Flag to indicate if a base color was defined
-
-
-def handled(func):
-    ''' Simple error handling
-    decorator to exit early '''
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            # Style the output for easier detection
-            print(e)
-            exit(1)
-    return wrapper
+    # }}}
 
 
 def main() -> None:
+    # main {{{
     arguments = set_arguments()
     configuration = set_configuration(arguments.file, arguments.style)
 
@@ -71,8 +67,10 @@ def main() -> None:
             shell=shell)
 
     def signal_trap(sig, frame) -> None:
-        ''' Some processes may hang, this
-        is to try and prevent that '''
+        """
+        Some processes may hang, this
+        is to try and prevent that
+        """
         process.kill()
         sys.exit(0)
 
@@ -86,10 +84,112 @@ def main() -> None:
         log_stderr(process.stderr, arguments)
 
     process.wait()
+    # }}}
+
+
+def colorize(text: str, color: str, style: Style) -> str:
+    # colorize {{{
+    match style:
+        case Style.Bit4:
+            return f"{CSI}{color}m{text}{RST_SUFFIX}"
+        case Style.Bit8:
+            return f"{CSI}38;5;{color}m{text}{RST_SUFFIX}"
+        case Style.Bit24:
+            r, g, b = color.split(",")
+            return f"{CSI}38;2;{r};{g};{b}m{text}{RST_SUFFIX}"
+    # }}}
+
+
+def handled(func):
+    """
+    Simple error handling
+    decorator to exit early
+    """
+    # handled {{{
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            # Style the output for easier detection
+            print(e)
+            exit(1)
+    return wrapper
+    # }}}
+
+
+def handle_line_mode(line: str, color: str, style: Style) -> None:
+    # handle_line_mode {{{
+    print(colorize(line, color, style), end="")
+    # }}}
+
+
+def handle_word_mode(line: str, key: str, color: str,
+                     style: Style, ignore: bool) -> str:
+    # handle_word_mode {{{
+    replace = colorize(key, color, style)
+    update = re.sub(key, replace, line) if ignore is False \
+        else re.sub(key, replace, line, flags=re.IGNORECASE)
+    return update
+    # }}}
+
+
+def log_stderr(pipe, arguments: Arguments) -> None:
+    # log_stderr {{{
+    if arguments.style == Style.Bit24:
+        red = "211,70,65"
+    elif arguments.style == Style.Bit8:
+        red = "160"
+    else:
+        red = "31"
+
+    for line in iter(pipe.readline, ""):
+        print(colorize(line, red, arguments.style), end="")
+    # }}}
+
+
+def log_stdout(pipe, configuration: Configuration,
+               arguments: Arguments) -> None:
+    # log_stdout {{{
+    for line in iter(pipe.readline, ""):
+        skip:   bool = False  # Flag for continuation
+        output: bool = False  # Flag for print responsibility
+
+        for index, key in enumerate(configuration.keywords):
+            pattern = re.compile(key) if arguments.ignore is False \
+                    else re.compile(key, re.IGNORECASE)
+
+            if re.search(pattern, line) is not None:
+                color = configuration.colors[index]
+                skip = True
+
+                if arguments.mode == Mode.LINE:
+                    handle_line_mode(line, color, arguments.style)
+                    break  # Word mode supports multiple-match
+                else:
+                    output = True
+                    # Key is needed to ensure all
+                    # occurrences are colored
+                    line = handle_word_mode(line, key, color,
+                                            arguments.style, arguments.ignore)
+
+        if output:  # Simple way to allow multiple-match
+            print(line, end="")
+
+        if skip:  # Cleaner than nested conditionals
+            continue
+
+        # Handle the base color case
+        if configuration.hasBase:
+            print(colorize(line, configuration.baseColor,
+                           arguments.style), end="")
+        else:
+            print(line, end="")
+    # }}}
 
 
 @handled
 def set_arguments() -> Arguments:
+    # set_arguments {{{
     description = "Capture and colorize log output"
     parser = argparse.ArgumentParser(description=description)
 
@@ -159,10 +259,12 @@ def set_arguments() -> Arguments:
     arguments.ignore = parsed_args.ignore
 
     return arguments
+    # }}}
 
 
 @handled
 def set_configuration(file: str, style: Style) -> Configuration:
+    # set_configuration {{{
     handle = open(file, "r")  # This may throw but is handled by decorator
     content = handle.readlines()
     configuration = Configuration([], [], 0, False)
@@ -205,79 +307,7 @@ def set_configuration(file: str, style: Style) -> Configuration:
             configuration.colors.append(color)
 
     return configuration
-
-
-def log_stdout(pipe, configuration: Configuration,
-               arguments: Arguments) -> None:
-    for line in iter(pipe.readline, ""):
-        skip:   bool = False  # Flag for continuation
-        output: bool = False  # Flag for print responsibility
-
-        for index, key in enumerate(configuration.keywords):
-            pattern = re.compile(key) if arguments.ignore is False \
-                    else re.compile(key, re.IGNORECASE)
-
-            if re.search(pattern, line) is not None:
-                color = configuration.colors[index]
-                skip = True
-
-                if arguments.mode == Mode.LINE:
-                    handle_line_mode(line, color, arguments.style)
-                    break  # Word mode supports multiple-match
-                else:
-                    output = True
-                    # Key is needed to ensure all
-                    # occurrences are colored
-                    line = handle_word_mode(line, key, color,
-                                            arguments.style, arguments.ignore)
-
-        if output:  # Simple way to allow multiple-match
-            print(line, end="")
-
-        if skip:  # Cleaner than nested conditionals
-            continue
-
-        # Handle the base color case
-        if configuration.hasBase:
-            print(colorize(line, configuration.baseColor,
-                           arguments.style), end="")
-        else:
-            print(line, end="")
-
-
-def handle_line_mode(line: str, color: str, style: Style) -> None:
-    print(colorize(line, color, style), end="")
-
-
-def handle_word_mode(line: str, key: str, color: str,
-                     style: Style, ignore: bool) -> str:
-    replace = colorize(key, color, style)
-    update = re.sub(key, replace, line) if ignore is False \
-        else re.sub(key, replace, line, flags=re.IGNORECASE)
-    return update
-
-
-def colorize(text: str, color: str, style: Style) -> str:
-    match style:
-        case Style.Bit4:
-            return f"{CSI}{color}m{text}{RST_SUFFIX}"
-        case Style.Bit8:
-            return f"{CSI}38;5;{color}m{text}{RST_SUFFIX}"
-        case Style.Bit24:
-            r, g, b = color.split(",")
-            return f"{CSI}38;2;{r};{g};{b}m{text}{RST_SUFFIX}"
-
-
-def log_stderr(pipe, arguments: Arguments) -> None:
-    if arguments.style == Style.Bit24:
-        red = "211,70,65"
-    elif arguments.style == Style.Bit8:
-        red = "160"
-    else:
-        red = "31"
-
-    for line in iter(pipe.readline, ""):
-        print(colorize(line, red, arguments.style), end="")
+    # }}}
 
 
 if __name__ == '__main__':
